328c13be189dab68046261da1d12e83a367a1148
Alex Ferreira <alex.ferreira1796@gmail.com>
2021-06-03 22:20:19 -0300

    removido console.log

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => { console.log("server up") });
+app.listen(port, () => {});
 
 // Rota principal
 app.get('/', (req: Request, res: Response) => {
